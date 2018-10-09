4d49aec22313548e138305e56cd83d1dcdf8a684
Dragos Sirbu <dragsirbu@gmail.com>
2018-10-09 20:02:02 +0200

    Finished Server Implementation

--- Session04/Ex04Sever/Program.cs
+++ Session04/Ex04Sever/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Ex04Sever
 {
@@ -9,30 +11,71 @@ namespace Ex04Sever
     {
         private const int PORT_NO = 5000;
         private const string SERVER_IP = "127.0.0.1";
+
+        private static void ProcessClientRequest(object argument)
+        {
+            TcpClient client = (TcpClient) argument;
+            try
+            {
+                var reader = new StreamReader(client.GetStream());
+                var writer = new StreamWriter(client.GetStream());
+                var message = String.Empty;
+                while (!(message = reader.ReadLine()).Equals("Exit") || (message == null))
+                {
+                    Console.WriteLine($"From client: {message}");
+                    writer.WriteLine($"From server: {message}");
+                    writer.Flush();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Problem with client communication. Exiting thread!");
+            }
+            finally
+            {
+                if (client != null)
+                    client.Close();
+            }
+
+        }
+        
         static void Main(string[] args)
         {
-            IPAddress address = IPAddress.Parse(SERVER_IP);
-            TcpListener listener = new TcpListener(address,PORT_NO);
-            
-            Console.WriteLine("Waiting for client...");
-            
-            listener.Start();
+            TcpListener listener = null;
 
-            TcpClient client = listener.AcceptTcpClient();
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse(SERVER_IP),PORT_NO);
+                listener.Start();
+                Console.WriteLine("Server started...");
+                while (true)
+                {
+                    Console.WriteLine("Waiting for incoming client connections..");
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine("Accepted new client connection...");
+                    Thread t = new Thread(ProcessClientRequest);
+                    t.Start(client);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
 
-            NetworkStream stream = client.GetStream();
-            var buffer = new byte[client.ReceiveBufferSize];
-
-            int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
-            var clientMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-            
-            Console.WriteLine($"Client: {clientMessage}");
-
-            var bytesToSend = ASCIIEncoding.ASCII.GetBytes("Hi client!");
-            stream.Write(bytesToSend,0,bytesToSend.Length); 
-           
-            
-            listener.Stop();
+//            NetworkStream stream = client.GetStream();
+//            var buffer = new byte[client.ReceiveBufferSize];
+//
+//            int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
+//            var clientMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+//            
+//            Console.WriteLine($"Client: {clientMessage}");
+//
+//            var bytesToSend = ASCIIEncoding.ASCII.GetBytes("Hi client!");
+//            stream.Write(bytesToSend,0,bytesToSend.Length); 
+//           
+//            
+//            listener.Stop();
             
             
         }
