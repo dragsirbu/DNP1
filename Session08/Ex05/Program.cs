49daa355c80888be6f1e468ace38085749ea8ec4
Dragos Sirbu <dragsirbu@gmail.com>
2018-10-30 19:19:33 +0100

    Finished Ex05

---
+++ Session08/Ex05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace Ex05
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static async Task DownloadPageAsync()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("Download-Agent","VIA Homepage");
+
+            var stringTask = client.GetStringAsync("http://www.via.dk");
+
+            var content = await stringTask;
+
+            Console.WriteLine(content);
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            DownloadPageAsync().Wait();
+            
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"\nTime to download: {elapsedMs} ms.");
+        }
+    }
+}
